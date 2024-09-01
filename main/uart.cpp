2b6e81987127998c8ff517a98896a9167c37df7a
theorlangur <theorlangur@gmail.com>
2024-09-01 15:10:26 +0200

    added class for uart communication

---
+++ main/uart.cpp
@@ -0,0 +1,189 @@
+#include "uart.hpp"
+#include <utility>
+
+namespace uart
+{
+    Channel::Channel(Port p, int baud_rate, Parity parity):
+        m_Port(uart_port_t(p)),
+        m_Config{
+            .baud_rate = baud_rate,
+            .data_bits = UART_DATA_8_BITS,
+            .parity    = (uart_parity_t)parity,
+            .stop_bits = UART_STOP_BITS_1,
+            .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+            .rx_flow_ctrl_thresh = 122,
+            .source_clk = UART_SCLK_DEFAULT,
+            .flags = {0}
+        }
+    {
+    }
+
+    Channel::~Channel()
+    {
+        Close();
+    }
+
+    Channel& Channel::SetBaudRate(int rate)
+    {
+        m_Config.baud_rate = rate;
+        return *this;
+    }
+
+    int Channel::GetBaudRate() const
+    {
+        return m_Config.baud_rate;
+    }
+
+    Channel& Channel::SetParity(Parity parity)
+    {
+        m_Config.parity = uart_parity_t(parity);
+        return *this;
+    }
+
+    Parity Channel::GetParity() const
+    {
+        return Parity{m_Config.parity};
+    }
+
+    Channel& Channel::SetDataBits(DataBits bits)
+    {
+        m_Config.data_bits = uart_word_length_t(bits);
+        return *this;
+    }
+
+    DataBits Channel::GetDataBits() const
+    {
+        return DataBits{m_Config.data_bits};
+    }
+
+    Channel& Channel::SetStopBits(StopBits bits)
+    {
+        m_Config.stop_bits = uart_stop_bits_t(bits);
+        return *this;
+    }
+
+    StopBits Channel::GetStopBits() const
+    {
+        return StopBits{m_Config.stop_bits};
+    }
+
+    Channel& Channel::SetHWFlowControl(HWFlowCtrl hw)
+    {
+        m_Config.flow_ctrl = uart_hw_flowcontrol_t(hw);
+        return *this;
+    }
+
+    HWFlowCtrl Channel::GetHWFlowControl() const
+    {
+        return HWFlowCtrl{m_Config.flow_ctrl};
+    }
+
+    Channel& Channel::SetRxBufferSize(int rx)
+    {
+        m_RxBufferSize = rx;
+        return *this;
+    }
+
+    int Channel::GetRxBufferSize() const
+    {
+        return m_RxBufferSize;
+    }
+
+    Channel& Channel::SetTxBufferSize(int tx)
+    {
+        m_TxBufferSize = tx;
+        return *this;
+    }
+
+    int Channel::GetTxBufferSize() const
+    {
+        return m_TxBufferSize;
+    }
+
+    Channel& Channel::SetQueueSize(int sz)
+    {
+        m_QueueSize = sz;
+        return *this;
+    }
+
+    int Channel::GetQueueSize() const 
+    {
+        return m_QueueSize;
+    }
+
+    Channel::ExpectedResult Channel::Configure()
+    {
+        CALL_ESP_EXPECTED("uart::Channel::Configure", uart_param_config(m_Port, &m_Config));
+        m_State.configured = true;
+        return std::ref(*this);
+    }
+
+    Channel::ExpectedResult Channel::SetPins(int tx, int rx, int rts, int cts)
+    {
+        if (!m_State.configured)
+            return std::unexpected(Err{"uart::Channel::SetPins", ESP_ERR_INVALID_STATE});
+
+        CALL_ESP_EXPECTED("uart::Channel::SetPins", uart_set_pin(m_Port, tx, rx, rts, cts));
+        m_State.pins_set = true;
+        return std::ref(*this);
+    }
+
+    Channel::ExpectedResult Channel::Open()
+    {
+        if (!m_State.pins_set)
+            return std::unexpected(Err{"uart::Channel::Open", ESP_ERR_INVALID_STATE});
+
+        CALL_ESP_EXPECTED("uart::Channel::Open", uart_driver_install(m_Port, m_RxBufferSize, m_TxBufferSize, m_QueueSize, &m_Handle, 0));
+        return std::ref(*this);
+    }
+
+    Channel::ExpectedResult Channel::Close()
+    {
+        m_StateU8 = 0;
+        return std::ref(*this);
+    }
+
+    Channel::ExpectedValue<size_t> Channel::GetReadyToReadDataLen()
+    {
+        size_t len;
+        CALL_ESP_EXPECTED("uart::Channel::GetReadyToReadDataLen", uart_get_buffered_data_len(m_Port, &len));
+        return RetVal<size_t>{*this, len};
+    }
+
+    Channel::ExpectedValue<size_t> Channel::GetReadyToWriteDataLen()
+    {
+        size_t len;
+        CALL_ESP_EXPECTED("uart::Channel::GetReadyToWriteDataLen", uart_get_tx_buffer_free_size(m_Port, &len));
+        return RetVal<size_t>{*this, len};
+    }
+
+    Channel::ExpectedResult Channel::Send(const uint8_t *pData, size_t len)
+    {
+        int r = uart_write_bytes(m_Port, pData, len);
+        if (r < 0)
+            return std::unexpected(Err{"uart::Channel::Send", ESP_ERR_INVALID_ARG});
+        if (r != len)
+            return std::unexpected(Err{"uart::Channel::Send", ESP_ERR_INVALID_SIZE});
+        return std::ref(*this);
+    }
+
+    Channel::ExpectedResult Channel::SendWithBreak(const uint8_t *pData, size_t len, size_t breakLen)
+    {
+        int r = uart_write_bytes_with_break(m_Port, pData, len, breakLen);
+        if (r < 0)
+            return std::unexpected(Err{"uart::Channel::Send", ESP_ERR_INVALID_ARG});
+        if (r != len)
+            return std::unexpected(Err{"uart::Channel::Send", ESP_ERR_INVALID_SIZE});
+
+        return std::ref(*this);
+    }
+
+    Channel::ExpectedValue<size_t> Channel::Read(uint8_t *pBuf, size_t len)
+    {
+        int r = uart_read_bytes(m_Port, pBuf, len, 0);
+        if (r < 0)
+            return std::unexpected(Err{"uart::Channel::Read", ESP_ERR_INVALID_ARG});
+
+        return RetVal<size_t>{*this, size_t(r)};
+    }
+}
---
+++ main/uart.hpp
@@ -0,0 +1,122 @@
+#ifndef UART_H_
+#define UART_H_
+
+#include "driver/uart.h"
+#include <expected>
+#include "generic_helpers.hpp"
+
+namespace uart
+{
+    enum class Port: std::underlying_type_t<uart_port_t>
+    {
+        Port0 = UART_NUM_0,
+        Port1 = UART_NUM_1,
+    };
+
+    enum class DataBits: std::underlying_type_t<uart_word_length_t>
+    {
+        Bits5 = UART_DATA_5_BITS,
+        Bits6 = UART_DATA_6_BITS,
+        Bits7 = UART_DATA_7_BITS,
+        Bits8 = UART_DATA_8_BITS,
+        BitsMax = UART_DATA_BITS_MAX
+    };
+
+    enum class StopBits: std::underlying_type_t<uart_stop_bits_t>
+    {
+        Bits1 = UART_STOP_BITS_1,
+        Bits1_5 = UART_STOP_BITS_1_5,
+        Bits2 = UART_STOP_BITS_2,
+        BitsMax = UART_STOP_BITS_MAX,
+    };
+
+    enum class Parity: std::underlying_type_t<uart_parity_t>
+    {
+        Disable = UART_PARITY_DISABLE,
+        Even = UART_PARITY_EVEN,
+        Odd = UART_PARITY_ODD,
+    };
+
+    enum class HWFlowCtrl: std::underlying_type_t<uart_hw_flowcontrol_t>
+    {
+        Disable = UART_HW_FLOWCTRL_DISABLE,
+        RTS = UART_HW_FLOWCTRL_RTS,
+        CTS = UART_HW_FLOWCTRL_CTS,
+        CTS_RTS = UART_HW_FLOWCTRL_CTS_RTS,
+        Max = UART_HW_FLOWCTRL_MAX
+    };
+
+    class Channel
+    {
+    public:
+        using Ref = std::reference_wrapper<Channel>;
+        using ExpectedResult = std::expected<Ref, Err>;
+
+        template<typename V>
+        struct RetVal
+        {
+            Ref c;
+            V v;
+        };
+
+        template<typename V>
+        using ExpectedValue = std::expected<RetVal<V>, Err>;
+
+        Channel(Port p, int baud_rate = 115200, Parity parity = Parity::Disable);
+        ~Channel();
+
+        Channel& SetBaudRate(int rate);
+        int GetBaudRate() const;
+
+        Channel& SetParity(Parity parity);
+        Parity GetParity() const;
+
+        Channel& SetDataBits(DataBits bits);
+        DataBits GetDataBits() const;
+
+        Channel& SetStopBits(StopBits bits);
+        StopBits GetStopBits() const;
+
+        Channel& SetHWFlowControl(HWFlowCtrl hw);
+        HWFlowCtrl GetHWFlowControl() const;
+
+        Channel& SetRxBufferSize(int rx);
+        int GetRxBufferSize() const;
+
+        Channel& SetTxBufferSize(int tx);
+        int GetTxBufferSize() const;
+
+        Channel& SetQueueSize(int sz);
+        int GetQueueSize() const;
+
+        ExpectedResult Configure();
+
+        ExpectedResult SetPins(int tx, int rx, int rts = UART_PIN_NO_CHANGE, int cts = UART_PIN_NO_CHANGE);
+
+        ExpectedResult Open();
+        ExpectedResult Close();
+
+        ExpectedValue<size_t> GetReadyToReadDataLen();
+        ExpectedValue<size_t> GetReadyToWriteDataLen();
+        
+        ExpectedResult Send(const uint8_t *pData, size_t len);
+        ExpectedResult SendWithBreak(const uint8_t *pData, size_t len, size_t breakLen);
+
+        ExpectedValue<size_t> Read(uint8_t *pBuf, size_t len);
+    private:
+        uart_port_t m_Port;
+        uart_config_t m_Config;
+        QueueHandle_t m_Handle = nullptr;
+        int m_RxBufferSize = 1024;
+        int m_TxBufferSize = 1024;
+        int m_QueueSize = 10;
+        union{
+            struct{
+                uint8_t configured: 1;
+                uint8_t pins_set: 1;
+            }m_State;
+            uint8_t m_StateU8 = 0;
+        };
+    };
+}
+#endif
