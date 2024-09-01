5256a5a90edf55b3365e6616dc8d0f7f0b84da5b
theorlangur <theorlangur@gmail.com>
2024-09-01 18:35:29 +0200

    added 'or_else'

--- main/functional_helpers.hpp
+++ main/functional_helpers.hpp
@@ -28,6 +28,18 @@ auto transform_error(T &&f)
     return transform_error_t{std::forward<T>(f)};
 }
 
+template<class V>
+struct or_else_t
+{
+    V t;
+};
+
+template<class V>
+auto or_else(V &&f)
+{
+    return or_else_t{std::move(f)};
+}
+
 template <typename T> struct get_arity : get_arity<decltype(&T::operator())> {};
 template <typename R, typename... Args> struct get_arity<R(*)(Args...)> : std::integral_constant<unsigned, sizeof...(Args)> {};
 // Possibly add specialization for variadic functions
@@ -43,11 +55,11 @@ auto invoke_continuation(std::expected<ExpVal, ExpErr> const &e, and_then_t<AndT
         static_assert(get_arity<AndThenV>::value <= std::tuple_size_v<ExpVal>, "Too many arguments");
         //destruct into separate
         return [&]<std::size_t... idx>(std::index_sequence<idx...>){
-            auto &v = e.value();
+            decltype(auto) v = std::move(e).value();
             return cont.t(std::get<idx>(v)...);
         }(std::make_index_sequence<get_arity<AndThenV>::value>{});
     }else if constexpr(get_arity<AndThenV>::value == 1)
-        return cont.t(e.value());
+        return cont.t(std::move(e).value());
     else
         return cont.t();
 }
@@ -56,9 +68,9 @@ template<class ExpVal, class ExpErr, class AndThenV>
 auto operator|(std::expected<ExpVal, ExpErr> &&e, and_then_t<AndThenV> &&cont)->decltype(invoke_continuation(std::declval<decltype(e)>(), std::declval<decltype(cont)>()))
 {
     if (!e)
-        return std::unexpected(e.error());
+        return std::unexpected(std::move(e).error());
 
-    return invoke_continuation(e, std::move(cont));
+    return invoke_continuation(std::move(e), std::move(cont));
 }
 
 template<class ExpVal, class ExpErr, class TransformErrV>
@@ -68,6 +80,15 @@ auto operator|(std::expected<ExpVal, ExpErr> &&e, transform_error_t<TransformErr
         return std::unexpected(tr_err.t(e.error()));
 
     return e.value();
+}
+
+template<class ExpVal, class ExpErr, class V>
+auto operator|(std::expected<ExpVal, ExpErr> &&e, or_else_t<V> &&def)->std::expected<ExpVal, ExpErr>
+{
+    if (!e)
+        return std::move(def).t;
+    else
+        return e;
 }
 
 #endif
