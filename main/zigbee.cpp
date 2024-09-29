810eaa6ab7498eca4344612e8c9715d745d1d75f
theorlangur <theorlangur@gmail.com>
2024-09-29 21:32:09 +0200

    zb: adding handler for zb signals (steering and whatnot)

--- main/zigbee.cpp
+++ main/zigbee.cpp
@@ -15,8 +15,15 @@ namespace zb
 
     static char g_Manufacturer[] = "Orlangur\0";
     static char g_Model[] = "Presence\0";
+    static const char *TAG = "ESP_ZB_PRESENCE_SENSOR";
 
-    void create_presence_ep(esp_zb_ep_list_t *ep_list, uint8_t ep_id)
+    struct APILock
+    {
+        APILock() { esp_zb_lock_acquire(portMAX_DELAY); }
+        ~APILock() { esp_zb_lock_release(); }
+    };
+
+    static void create_presence_ep(esp_zb_ep_list_t *ep_list, uint8_t ep_id)
     {
         esp_zb_basic_cluster_cfg_t basic_cfg =                                                                                \
             {                                                                                       
@@ -52,6 +59,59 @@ namespace zb
             .app_device_version = 0
         };
         esp_zb_ep_list_add_ep(ep_list, cluster_list, endpoint_config);
+    }
+
+    static void bdb_start_top_level_commissioning_cb(uint8_t mode_mask)
+    {
+        ESP_RETURN_ON_FALSE(esp_zb_bdb_start_top_level_commissioning(mode_mask) == ESP_OK, ,
+                            TAG, "Failed to start Zigbee bdb commissioning");
+    }
+
+    void esp_zb_app_signal_handler(esp_zb_app_signal_t *signal_struct)
+    {
+        uint32_t *p_sg_p     = signal_struct->p_app_signal;
+        esp_err_t err_status = signal_struct->esp_err_status;
+        esp_zb_app_signal_type_t sig_type = *(esp_zb_app_signal_type_t*)p_sg_p;
+        switch (sig_type) {
+        case ESP_ZB_ZDO_SIGNAL_SKIP_STARTUP:
+            //ESP_LOGI(TAG, "Initialize Zigbee stack");
+            esp_zb_bdb_start_top_level_commissioning(ESP_ZB_BDB_MODE_INITIALIZATION);
+            break;
+        case ESP_ZB_BDB_SIGNAL_DEVICE_FIRST_START:
+        case ESP_ZB_BDB_SIGNAL_DEVICE_REBOOT:
+            if (err_status == ESP_OK) {
+                //ESP_LOGI(TAG, "Deferred driver initialization %s", deferred_driver_init() ? "failed" : "successful");
+                //init ld2412 and other sensors
+                ESP_LOGI(TAG, "Device started up in %s factory-reset mode", esp_zb_bdb_is_factory_new() ? "" : "non");
+                if (esp_zb_bdb_is_factory_new()) {
+                    ESP_LOGI(TAG, "Start network steering");
+                    esp_zb_bdb_start_top_level_commissioning(ESP_ZB_BDB_MODE_NETWORK_STEERING);
+                } else {
+                    ESP_LOGI(TAG, "Device rebooted");
+                }
+            } else {
+                /* commissioning failed */
+                ESP_LOGW(TAG, "Failed to initialize Zigbee stack (status: %s)", esp_err_to_name(err_status));
+            }
+            break;
+        case ESP_ZB_BDB_SIGNAL_STEERING:
+            if (err_status == ESP_OK) {
+                esp_zb_ieee_addr_t extended_pan_id;
+                esp_zb_get_extended_pan_id(extended_pan_id);
+                ESP_LOGI(TAG, "Joined network successfully (Extended PAN ID: %02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x, PAN ID: 0x%04hx, Channel:%d, Short Address: 0x%04hx)",
+                         extended_pan_id[7], extended_pan_id[6], extended_pan_id[5], extended_pan_id[4],
+                         extended_pan_id[3], extended_pan_id[2], extended_pan_id[1], extended_pan_id[0],
+                         esp_zb_get_pan_id(), esp_zb_get_current_channel(), esp_zb_get_short_address());
+            } else {
+                ESP_LOGI(TAG, "Network steering was not successful (status: %s)", esp_err_to_name(err_status));
+                esp_zb_scheduler_alarm((esp_zb_callback_t)bdb_start_top_level_commissioning_cb, ESP_ZB_BDB_MODE_NETWORK_STEERING, 1000);
+            }
+            break;
+        default:
+            ESP_LOGI(TAG, "ZDO signal: %s (0x%x), status: %s", esp_zb_zdo_signal_to_string(sig_type), sig_type,
+                     esp_err_to_name(err_status));
+            break;
+        }
     }
 
     void zigbee_main()
