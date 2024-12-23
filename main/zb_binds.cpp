c343abd69245033e3df1b6c2b367d8baa1fba92c
theorlangur <theorlangur@gmail.com>
2024-12-23 21:47:28 +0100

    started writing obtaining of a binding table of the bound on/off server device

--- main/zb_binds.cpp
+++ main/zb_binds.cpp
@@ -32,5 +32,19 @@ namespace zb
 
     void BindInfo::GetBindTable()
     {
+        esp_zb_zdo_mgmt_bind_param_t cmd_req;
+        cmd_req.dst_addr = m_ShortAddr;
+        cmd_req.start_index = 0;
+
+        FMT_PRINT("Sending request to {:x} to get binds\n", cmd_req.dst_addr);
+        esp_zb_zdo_binding_table_req(&cmd_req, 
+            [](const esp_zb_zdo_binding_table_info_t *table_info, void *user_ctx)
+            {
+                BindInfo *pBind = (BindInfo *)user_ctx;
+                if (!g_BindInfoPool.IsValid(pBind)) return;//we're dead
+
+
+                //TODO: implement
+            }, this);
     }
 }
